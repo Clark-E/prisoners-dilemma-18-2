842acf7ab365c13cde3c27dfcc838ac64cf8763a
aharp01 <allymharp@gmail.com>
2018-01-18 07:43:51 -0500

    team 2 change 2
    change 2

--- team2.py
+++ team2.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -7,8 +8,8 @@
 ####
 
 team_name = 'BACH' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'collude for 30, then decide'
+strategy_description = 'collude for 30, then decide'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,8 +27,56 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
+    # This strategy is completely random.
+    '''
+    number = random.randInt(0, 1)
+    if number = 0:
+        return 'c'
+    else:
+        return 'b'
+    '''
+    # This strategy runs n trials, then decides whether to collude or betray.
+    '''
+    betrayal_count = 0
+    collusion_count = 0
+    if len(my_history) < 15:
+        return 'c'
+    elif len(my_history) == 15:
+        for i in range(len(their_history)):
+            if i == 'c':
+                collusion_count += 1
+            elif i == 'b':
+                betrayal_count += 1
+    if betrayal_count > collusion_count:
+        return 'b'
+    elif collusion_count > betrayal_count:
+        return 'c'         
+    else:
+        number = random.randint(0, 1)
+        if number == 0:
+            return 'c'
+        else:
+            return 'b'          
+    '''
+    
+    # This strategy colludes until betrayed.
+    
+    betrayal_count = 0
+    collusion_count = 0
+    for i in range(len(their_history)):
+            if i == 'c':
+                collusion_count += 1
+            elif i == 'b':
+                betrayal_count += 1
+    if collusion_count == 0:
+        return 'c'
+    else:
+        return 'b'
+    
+    
+    '''
     return 'c'
-
+    '''
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
