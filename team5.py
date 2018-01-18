df137fd4689f492573ba8ebc7a62ea9688135579
HyN14 <hytheamazing73@gmail.com>
2018-01-18 08:08:48 -0500

    Code thingy

--- team5.py
+++ team5.py
@@ -7,16 +7,19 @@
 ####
 
 team_name = 'SpeedBoi' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+strategy_name = 'Radomize it'
+strategy_description = 'Randomize between b and c'
+
+'''   
+Plan A
+------
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
+    Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
     Make my move.
     Returns 'c' or 'b'. 
-    '''
+    
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,9 +29,30 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    return 'b'
+'''
+'''
+Plan B
+--------
+roun = 0
+def move(my_history, their_history, my_score, their_score):
+    global roun
+    roun = roun + 1
+    if(roun %2 == 0):
+        return 'b'
+    else:
+        return 'c'
 
-    
+'''
+
+def move(my_history, their_history, my_score, their_score): 
+    import random
+    er = random.randint(1, 2)
+    if er == 1:
+        return 'c'
+    else:
+        return 'b'
+
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -65,4 +89,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')
