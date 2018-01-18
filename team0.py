ac18c97a6c75472e48db1ad6d31f9378956f1d53
mcavoy-katie <katiermcavoy@gmail.com>
2018-01-18 08:04:49 -0500

    Revert "Changed the definition of Move."
    This reverts commit 0e0479670ad9978561a3df2f662f7630aa377da7.

--- team0.py
+++ team0.py
@@ -5,10 +5,10 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-# import random
+
 team_name = 'kmja0' # Only 10 chars displayed.
-strategy_name = 'Collude, unless betrayed.'
-strategy_description = 'This strategy colludes every time unless betrayed.'
+strategy_name = 'The name the team gives to this strategy'
+strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,25 +25,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    '''
-    number = random.randInt(1, 200)
-    if number == 5:
-        return 'b'
-    else:
-        return 'c'
-            '''
-    if len(my_history) == 0:
-        return 'c'
-    elif their_history[-1] == 'b':
-        return 'b'
-    else:
-        return 'c'
-        '''
-    if their_history[-1] == 'b':
-        return 'b'
-    else:
-        return 'c'
-        '''
+    
+    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
